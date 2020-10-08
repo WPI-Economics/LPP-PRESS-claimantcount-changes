dd728c5f25a8dd0546b6f6e2b6e862599d06b26b
guyweir <guy.weir@wpieconomics.com>
2020-10-08 16:07:05 +0100

    gets the map working

--- Analysis.R
+++ Analysis.R
@@ -2,6 +2,7 @@
 
 library(nomisr)
 library(tidyverse)
+library(readxl)
 
 ################# Import and process old CC ##########################
 
@@ -111,3 +112,197 @@ cc <- cc %>% rename("lsoa11cd" = area,
 cc$`Claimant count (August 2017)` <- format(cc$`Claimant count (August 2017)`, big.mark = ",")
 cc$`Claimant count (August 2020)` <- format(cc$`Claimant count (August 2020)`, big.mark = ",")
 cc$`Population 2018` <- format(cc$`Population 2018`, big.mark = ",")
+
+EWlook <- read_csv("os lsoa msoa lookup.csv")
+EWScotlook <- EWlook %>% select(LSOA11CD, LSOA11NM,LAD17NM, RGN11NM)
+EWScotlook <- EWScotlook[!duplicated(EWScotlook),]
+# EWScotlook <- bind_rows(EWlook, scotlook)
+EWScotlook <- rename(EWScotlook,"Local Authority" = LAD17NM, "Region/Country" = RGN11NM, "Neighbourhood name" = LSOA11NM)
+
+cc <- merge(cc, EWScotlook, by.x ="lsoa11cd", by.y = "LSOA11CD" )
+cc <- select(cc,-`Population 2018`)
+
+###### ADD IMD
+
+#' England
+IMD19Eng <- read_excel("File_2_-_IoD2019_Domains_of_Deprivation.xlsx", 
+                       sheet = "IoD2019 Domains")
+IMD19Eng <- IMD19Eng %>% select(`LSOA code (2011)`,
+                                `Local Authority District name (2019)`,
+                                `Local Authority District code (2019)`, 
+                                `Index of Multiple Deprivation (IMD) Rank (where 1 is most deprived)`,
+                                `Index of Multiple Deprivation (IMD) Decile (where 1 is most deprived 10% of LSOAs)`) %>% 
+  rename(`IMD Rank (1 is most deprived)` = `Index of Multiple Deprivation (IMD) Rank (where 1 is most deprived)`,
+         `IMD decile (1 is most deprived)` = `Index of Multiple Deprivation (IMD) Decile (where 1 is most deprived 10% of LSOAs)`
+  )
+
+
+cc <- merge(cc, IMD19Eng[c(1,4,5)], by.x = "lsoa11cd", by.y = "LSOA code (2011)", all.x = T)
+cc$`IMD rank London (1 is most deprived)` <- round(rank(cc$`IMD Rank (1 is most deprived)`),0)
+#rebased deciles
+cc <- cc %>% mutate(`IMD decile London (1 is most deprived)` = ntile(`IMD rank London (1 is most deprived)`,10))
+
+#remove England ranks
+cc <- select(cc,-`IMD Rank (1 is most deprived)`,-`IMD decile (1 is most deprived)`,-`Region/Country`)
+
+
+cc <- cc[,c(1,8,9,11,10,2,3,4,5,6,7)]
+
+###########################################
+################### TABLE ################
+###########################################
+library(reactable)
+library(crosstalk)
+
+
+table <- cc #%>% select(-lsoa11cd)
+
+
+#table1 <- table[!is.na(table$`Change decile (1 = low)`),]
+
+
+table2 <- SharedData$new(table, group = "1")
+
+
+##########the reactable table
+
+tbl <- reactable(table2, selection = "multiple",
+                 onClick = "select",
+                 rowStyle = list(cursor = "pointer"),
+                 minRows = 10,filterable = F,searchable = F, wrap = T , defaultPageSize = 15, striped = T, highlight = T,
+                 defaultSorted = list("Claimant count rate ppt change (August 2017 - August 2020)" = "desc"),
+                 columns = list(`Change decile (1 = low)` = colDef(filterable = T),
+                                `Local Authority` = colDef(filterable = T),
+                                `IMD decile London (1 is most deprived)` = colDef(filterable = T)),
+                 #`COVID-19 deaths per 100,000` = colDef(aggregate = "mean",format = colFormat(digits = 0)),
+                 #`COVID-19 deaths age adjusted per 100,000` = colDef(aggregate = "mean",format = colFormat(digits = 0))),
+                 theme = reactableTheme(
+                   stripedColor = "#faf8f1",
+                   highlightColor = "#e5dec4",
+                   cellPadding = "6px 10px",
+                   style = list(fontFamily = "Arial", fontSize = "12px"),
+                   #searchInputStyle = list(width = "100%", fontWeight = "400"),
+                   headerStyle = list(color = "white",background = "#2A2A2A",
+                                      "&:hover[aria-sort]" = list(background = "#8c8c8c "),
+                                      "&[aria-sort='ascending'], &[aria-sort='descending']" = list(background = "#8c8c8c"),
+                                      borderColor = "#555"
+                   )
+                 )) 
+tbl
+
+######################################
+############## MAP ###################
+#####################################
+
+library(rgdal)
+library(rgeos)
+suppressPackageStartupMessages(library(sp))
+suppressPackageStartupMessages(library(sf))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(ggiraph))
+suppressPackageStartupMessages(library(geojsonio))
+suppressPackageStartupMessages(library(leaflet))
+suppressPackageStartupMessages(library(leaflet.extras))
+
+lsoa.centroids <- geojson_sf("https://opendata.arcgis.com/datasets/b7c49538f0464f748dd7137247bbc41c_0.geojson")
+
+#merge in the cc data
+#EWS.centroids <- st_as_sf(EWS.centroids)
+EWS.centroids.df <- merge(cc,lsoa.centroids , by = "lsoa11cd", all.x = T)
+EWS.centroids.df <- st_as_sf(EWS.centroids.df)
+
+EWS.centroids.df$ccradius <- (EWS.centroids.df$`Claimant count rate ppt change (August 2017 - August 2020)`)* 0.5 + 2
+EWS.centroids.df$"Change decile (1 = low)" <- as.factor(EWS.centroids.df$"Change decile (1 = low)")
+#EWS.centroids.df <- EWS.centroids.df[c(1:14,16,15)]
+
+factpal <- colorFactor("RdBu",levels = levels(EWS.centroids.df$`Change decile (1 = low)`[!is.na(EWS.centroids.df$`Change decile (1 = low)`)]), 
+                       ordered = TRUE, reverse = T )
+
+
+labels <- sprintf("<strong>%s</strong><br/>%g ppt change<sup></sup>",
+                  EWS.centroids.df$`Neighbourhood name`, round(EWS.centroids.df$`Claimant count rate ppt change (August 2017 - August 2020)`,1)) %>% 
+  lapply(htmltools::HTML)
+
+
+
+
+
+#function for adding circle sizes to the legend
+addLegendCustom <- function(map, colors, labels, sizes, opacity = 0.5, position){
+  
+  colorAdditions <- paste0(colors, "; border-radius: 50%; width:", sizes, "px; height:",
+                           sizes, "px", "; position: relative; left: ",max(sizes)-(sizes/2)-12,"px")
+  
+  labelAdditions <- paste0("<div style='display: inline-block;height: ", 
+                           sizes,";position:relative; left: ",max(sizes)-(sizes),"px","; bottom: ",
+                           10,"px",";margin-top: 12px;line-height: ", sizes, "px;'>", 
+                           labels, "</div>")
+  
+  return(addLegend(map, colors = colorAdditions, 
+                   labels = labelAdditions, opacity = opacity, position = position))
+}
+
+#add title to page
+library(htmltools)
+
+#page element title
+title <- tags$div(HTML("Claimant count percentage point change,<br> August 2017 to August 2020, Great Britain</br>"), 
+                  style = "font-family: Open Sans;color: #2A2A2A;font-weight: bold; font-size: 22px; text-align: center"
+)
+
+#page element data sources
+sources <- tags$div(HTML("Claimant Count, ONS<br> Analysis: WPI Economics on behalf of CRC"), 
+                    style = "font-family: Open Sans;color: #2A2A2A;font-style: italic; font-size: 12px; text-align: left"
+)
+
+#remove NA
+#EWS.centroids.df <- EWS.centroids.df[!is.na(EWS.centroids.df$`Change decile (1 = low)`),]
+#EWS.centroids.df <- st_as_sf(EWS.centroids.df)
+
+EWS.centroids.dfXT <- SharedData$new(EWS.centroids.df, group = "1") 
+
+
+#get region layer to make clip layer
+NUTS1 <- geojson_sf("https://opendata.arcgis.com/datasets/01fd6b2d7600446d8af768005992f76a_3.geojson")
+NUTS1 <- NUTS1 %>% filter(nuts118nm != "London")
+
+
+
+#map element
+m2 <- leaflet(EWS.centroids.dfXT, height = "580px", options = list(padding = 100)) %>% setView(-0.035,51.489, 9.5) %>% 
+  setMapWidgetStyle(list(background = "white")) %>% addProviderTiles(providers$CartoDB.Positron, providerTileOptions(opacity = 1) ) %>% 
+  
+  
+  addCircleMarkers( group = "circlegw",
+                    radius = ~`ccradius`,
+                    stroke = F,
+                    color = ~factpal(`Change decile (1 = low)`), opacity = 0.85, fillOpacity = 0.85,
+                    label = labels,
+                    labelOptions = labelOptions(
+                      style = list("font-weight" = "normal", padding = "3px 8px"),
+                      textsize = "15px",
+                      direction = "auto"))   %>% 
+  
+  addPolygons(data = NUTS1, color = "white",opacity = 1, fillColor = "white", fillOpacity = 1)  %>%        #add london clip
+  
+  addLegendCustom(colors = c("grey", "grey", "grey"), 
+                  labels = c("+1ppts","+5ppts","+10ppts"),
+                  
+                  sizes = c(2.5,4.5,7)*2, position = "bottomright" ) %>% 
+  
+  addLegend(pal = factpal, values = EWS.centroids.df$`Change decile (1 = low)`, 
+            labels = levels(EWS.centroids.df$`Change decile (1 = low)`), position = "bottomright", title = "Deciles <br>(1 = low)") %>% 
+  removeDrawToolbar(clearFeatures = T) %>% 
+  addResetMapButton() 
+m2
+
+
+
+
+
+combo <- htmltools::tagList(m2, tbl,sources) #I think this makes a combined html object
+#browsable(combo)
+
+############# Move index.html and lib folder manually into /docs htmltools doesn't support detailed file paths :( )
+htmltools::save_html(combo, "index.html", background = "#FFFCF1") #this saves it as an HTML page in the default folder.
+
